60a4867147a79c79265c6e6f693040cae780cb68
Jerry Davidson <willdjerry@gmail.com>
2019-09-07 21:45:23 -0400

    nav component has commented out temp links, for now the website doesnt need any user navigation links

--- src/components/home-page/HomePage.js
+++ src/components/home-page/HomePage.js
@@ -1,12 +1,20 @@
 import React from "react";
-import './HomePage.css'
+import Nav from "../nav/Nav";
+import "./HomePage.css";
 
 function HomePage() {
   return (
     <div className="home-page">
-      <div className='homepage-title'>
+        <div className = 'top-container'>
+      <div className="homepage-title">
         <h2>trnd</h2>
       </div>
+      <div className='nav-container'>
+        <Nav />
+      </div>
+
+        </div>
+
     </div>
   );
 }
---
+++ src/components/nav/Nav.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Nav.css";
+
+function Nav() {
+  return (
+    <div className="nav">
+      <div className="username">
+        <h2>JWillD</h2>
+      </div>
+      {/* <div className="home-button">
+        <h2>Home</h2>
+      </div> */}
+      {/* <div className="temp-nav-one">
+        <h2>temp</h2>
+      </div>
+      <div className="temp-nav-two">
+        <h2>temp</h2>
+      </div> */}
+    </div>
+  );
+}
+
+export default Nav;
