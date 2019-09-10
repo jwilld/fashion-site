694308cdfe44fe23990827803395d36a90321f08
Jerry Davidson <willdjerry@gmail.com>
2019-09-10 10:18:48 -0400

    basic outline for loginform show the text "login form" and an input field

---
+++ src/components/acccess-forms/Login.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Login.css'
+
+function LoginForm(){
+    return(
+        <div>
+            <h1>Login form</h1>
+            <input></input>
+        </div>
+    )
+}
+
+export default LoginForm
+
+
+
+
+
