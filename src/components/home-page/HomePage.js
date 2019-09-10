f43c8c83f3313affc30180547229f9393d21377b
Jerry Davidson <willdjerry@gmail.com>
2019-09-10 10:11:39 -0400

    access forms added to components folder.

--- src/components/home-page/HomePage.js
+++ src/components/home-page/HomePage.js
@@ -1,20 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
+import { Link } from "react-router-dom";
 import Nav from "../nav/Nav";
 import "./HomePage.css";
 import TrndDisplay from "../trnd-display/TrndDisplay";
 
 function HomePage() {
+
   return (
     <div className="home-page">
       <div className="top-container">
         <div className="homepage-title">
-          <h2>trnd</h2>
+          <Link to ='/' className='title-nav-text'>
+            <h2>trnd</h2>
+          </Link>
         </div>
         <div className="nav-container">
           <Nav />
         </div>
       </div>
-      <div className= 'trnd-container'>
+      <div className="trnd-container">
         <TrndDisplay />
       </div>
     </div>
